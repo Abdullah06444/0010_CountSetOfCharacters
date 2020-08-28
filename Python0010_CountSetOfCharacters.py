402d57eb3ec53a8252cd46c25ffdfa2a6ab9d6ec
Abdullah06444 <abdullah44gur@gmail.com>
2020-08-29 00:34:29 +0300

    initial commit

--- Python0010_CountSetOfCharacters.py
+++ Python0010_CountSetOfCharacters.py
@@ -2,25 +2,17 @@ counter = 0
 i = 0
 ch = str(input())
 result = ""
-print("string => ", ch, " length => ", len(ch))
 
 for i in range(len(ch)):
 
     if ch[i] != ' ':
         character = ch[i]
-        if character != 'a' and character != 'e' and character != 'i' and character != 'o' and character != 'u' \
-        and character != 'A' and character != 'E' and character != 'I' and character != 'O' and character != 'U':
-            print("4uuuuuuu {0} ".format(counter))
-            print("result ", result, " char ", character)
-            if character < 91:
-                result.append(character)
-            #if character < 91 or character > 96:
-            #   result.append(character)
+        if character >= 'A' and character <= 'Z' or character >= 'a' and character <= 'z':
+            if character != 'a' and character != 'e' and character != 'i' and character != 'o' and character != 'u' \
+            and character != 'A' and character != 'E' and character != 'I' and character != 'O' and character != 'U':
+                if character <= 'Z' or character >= 'a':
+                    result += character
+            counter += 1
+#            print("counter= {0}".format(counter),result)
 
-
-    print("i => ", i)
-    counter += 1
-
-
-print("{0} ".format(counter))
-print(result.upper())
+print("{0}".format(counter),result.upper())
